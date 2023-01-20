08d3f212752dc75dd89548ba8815905a0a1ac989
Ethan Davidson <edavidson@leagueapps.com>
2023-01-20 10:56:31 -0500

    get sample working with python3
    also:
    - add requirements.txt
    - add .p12 and .pem to gitignore
    - clean up code and reformat

--- sample.py
+++ sample.py
@@ -4,8 +4,7 @@
 # results.  A limited number of results are returned in each response.  It can
 # vary based on the type, but is generally around 1000 records.
 
-# ubuntu 16.04: sudo apt install python-jwt python-crypto python-requests
-# untested: pip install pyjwt crypto requests2
+# install dependencies with: python -m pip install -r requirements.txt
 
 import argparse
 import time
@@ -16,55 +15,59 @@ import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--site-id', type=int, required=True)
-parser.add_argument('--client-id', required=True, help='client id for site.  Probably the same as the certificate filename basename')
+parser.add_argument('--client-id', required=True,
+                    help='client id for site.  Probably the same as the certificate filename basename')
 parser.add_argument('--pem-file', required=True, help='filename for certificate key in PEM format')
-parser.add_argument('--type', required=True, choices=['registrations-2','members-2','transactions-2', 'accountingCodes'], help='type of records to export')
+parser.add_argument('--type', required=True,
+                    choices=['registrations-2', 'members-2', 'transactions-2', 'accountingCodes'],
+                    help='type of records to export')
 parser.add_argument('--domain', default='leagueapps.io')
 parser.add_argument('--auth', default='https://auth.leagueapps.io')
 args = parser.parse_args()
 
 if args.auth:
     print("using auth server {}".format(args.auth))
-    auth_host=args.auth
+    auth_host = args.auth
+else:
+    auth_host = 'https://auth.leagueapps.io'
 
 if args.domain == 'lapps-local.io':
     # for local testing the Google ESP isn't HTTPS
-    admin_host='http://admin.{}:8082'.format(args.domain)
+    admin_host = 'http://admin.{}:8082'.format(args.domain)
 else:
-    admin_host='https://admin.{}'.format(args.domain)
+    admin_host = 'https://admin.{}'.format(args.domain)
 
-site_id=args.site_id
-record_type=args.type
+site_id = args.site_id
+record_type = args.type
 
 
 # Make a request to the OAuth 2 token endpoint with a JWT assertion to get an
 # access_token
-def request_access_token(auth_host, client_id, pem_file):
+def request_access_token(auth_host_url, client_id, pem_file):
     with open(pem_file, 'r') as f:
         key = f.read()
 
     now = int(time.time())
+    auth_url = '{}/v2/auth/token'.format(auth_host_url)
 
     claims = {
-            'aud': 'https://auth.leagueapps.io/v2/auth/token',
-            'iss': client_id,
-            'sub': client_id,
-            'iat': now,
-            'exp': now + 300
-            }
+        'aud': 'https://auth.leagueapps.io/v2/auth/token',
+        'iss': client_id,
+        'sub': client_id,
+        'iat': now,
+        'exp': now + 300
+    }
 
     assertion = jwt.encode(claims, key, algorithm='RS256')
 
-    auth_url = '{}/v2/auth/token'.format(auth_host)
+    resp = requests.post(auth_url,
+                         data={'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                               'assertion': assertion})
 
-    response = requests.post(auth_url,
-            data={ 'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-                'assertion': assertion })
-
-    if response.status_code == 200:
-        return response.json()['access_token']
+    if resp.status_code == 200:
+        return resp.json()['access_token']
     else:
-        print('failed to get access_token: ({}) {}'.format(response.status_code, response.text))
+        print('failed to get access_token: ({}) {}'.format(resp.status_code, resp.text))
         return None
 
 
@@ -74,11 +77,11 @@ def request_access_token(auth_host, client_id, pem_file):
 # random calculated slot to get the total sleep time.
 #
 # max_slots can be used to put an upper bound on the sleep time
-def exponential_backoff(attempts, slot_time = 1, max_slots = 0):
+def exponential_backoff(attempts_so_far, slot_time=1.0, max_slots=0):
     if max_slots > 0:
-        attempts = min(attempts, max_slots)
+        attempts_so_far = min(attempts_so_far, max_slots)
 
-    return random.randint(0, 2 ** attempts - 1) * slot_time
+    return random.randint(0, 2 ** attempts_so_far - 1) * slot_time
 
 
 # Initialize the last-updated and last-id query parameters to be used between
@@ -91,8 +94,8 @@ access_token = None
 batch_count = 0
 
 # Maximum number of retries for a request
-max_attempts=5
-attempts=0
+max_attempts = 5
+attempts = 0
 while attempts < max_attempts:
     attempts += 1
 
@@ -105,11 +108,23 @@ while attempts < max_attempts:
 
     print('access token: {}'.format(access_token))
 
-    params={'last-updated': last_updated, 'last-id': last_id}
+    params = {'last-updated': last_updated, 'last-id': last_id}
     # set the access token in the request header
-    headers={ 'authorization': 'Bearer {}'.format(access_token) }
+    headers = {'authorization': 'Bearer {}'.format(access_token)}
 
-    response = requests.get('{}/v2/sites/{}/export/{}'.format(admin_host, site_id, record_type), params=params, headers=headers)
+    if record_type == 'accountingCodes':  # accountingCodes endpoint doesn't have /export/ in it
+        url = '{}/v2/sites/{}/{}'.format(admin_host, site_id, record_type)
+    else:
+        url = '{}/v2/sites/{}/export/{}'.format(admin_host, site_id, record_type)
+
+    try:
+        response = requests.get(url, params=params,
+                                headers=headers, timeout=10)
+    except requests.exceptions.Timeout:
+        wait_seconds = exponential_backoff(attempts, 1.42, 5)
+        print('retry in {} seconds due to timeout'.format(wait_seconds))
+        time.sleep(wait_seconds)
+        continue
 
     # access_token is invalid, clear so next pass through the loop will get a new one
     if response.status_code == 401:
@@ -149,17 +164,24 @@ while attempts < max_attempts:
     print('processing batch {}, {} records'.format(batch_count, len(records)))
     printFile = open("records.json", "w+")
 
+
     def remove_uni(s):
         s2 = s.replace("u'", "'")
         s2 = s2.replace('u"', '"')
         return s2
+
+
     printFile.write("[")
     for record in records:
-        #print('record id: {}, {}'.format(record['id'], record['lastUpdated']))
+        # print('record id: {}, {}'.format(record['id'], record['lastUpdated']))
         # track last_updated and last_id so next request will fetch more records
-        last_updated = record['lastUpdated']
-        last_id = record['id']
+        if record_type != 'accountingCodes':  # accountingCodes endpoint doesn't return this data
+            last_updated = record['lastUpdated']
+            last_id = record['id']
         printFile.write(remove_uni(str(record)) + ",")
 
     printFile.write("]")
     printFile.close()
+
+    if record_type == 'accountingCodes':
+        break  # accountingCodes endpoint is not paginated, so no need to loop
